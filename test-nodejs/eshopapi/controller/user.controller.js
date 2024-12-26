37d73162ae191972122929efe504283423c1f28c
mean <meanstack4@gmail.com>
2024-12-26 15:29:37 +0530

    jonwebtoken and dom

--- test-nodejs/eshopapi/controller/user.controller.js
+++ test-nodejs/eshopapi/controller/user.controller.js
@@ -1,20 +1,26 @@
 import { validationResult } from "express-validator"
 import { User } from "../model/user.model.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 export const signIn = async (request,response,next)=>{
    try{
     let {email,password} = request.body;
     let user = await User.findOne({email});
     if(user){
       let status = bcrypt.compareSync(password,user.password);
-      return status ? response.status(200).json({message: "Sign in success..",user}) : response.status(401).json({error: "Bad request | invalid password"})
+      return status ? response.status(200).json({message: "Sign in success..",user,token: generateToken(user._id)}) : response.status(401).json({error: "Bad request | invalid password"})
     }
     else
      return response.status(401).json({error: "Bad request | invalid email id"});
    }
    catch(err){
+    console.log(err);
     return response.status(500).json({error: "Internal Server Error"});
    }
+}
+const generateToken = (userId)=>{
+   let token = jwt.sign({payload: userId},"fsdfsdrereioruxvxncnv");
+   return token; 
 }
 export const signUp = async (request,response,next)=>{
   try{  
--- test-nodejs/eshopapi/routes/cart.route.js
+++ test-nodejs/eshopapi/routes/cart.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { addToCart, fetchCart, removeItem } from "../controller/cart.controller.js";
+import { auth } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post("/add-to-cart",addToCart);
+router.post("/add-to-cart",auth,addToCart);
 
-router.get("/:id",fetchCart);
-router.delete("/remove-item/:userId/:productId",removeItem);
+router.get("/:id",auth,fetchCart);
+router.delete("/remove-item/:userId/:productId",auth,removeItem);
 export default router;
